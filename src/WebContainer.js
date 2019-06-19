6e62bf8bd23f1ba5c1792d3c1d6ff94e60c65b66
mattbf <matthewbf8@gmail.com>
2019-06-19 16:01:25 -0600

    Iframe doesn't load CORS restricted elements

--- src/WebContainer.js
+++ src/WebContainer.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import WebContainer from './WebContainer.js';
+import Iframe from 'react-iframe'
+// var url = data.url + "&output=embed";
+// window.location.replace(url);
 
 function WebContainer() {
   return (
     <div>
-      Web Container
+      <Iframe
+        url="http://www.google.com&output=embed"
+        src="http://www.google.com&output=embed"
+        width="500px"
+        height="450px"
+        id="myId"
+        className="myClassname"
+        display="initial"
+        position="relative"
+        sandbox="allow-same-origin allow-forms"
+        scrolling
+      />
     </div>
   );
 }
