1a1aa39b0f37fd694bed2fd4c6f75be7a8105b3f
mattbf <matthewbf8@gmail.com>
2019-06-19 10:48:01 -0600

    Setting up repo and adding base component

---
+++ src/WebContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WebContainer from './WebContainer.js';
+
+function WebContainer() {
+  return (
+    <div>
+      Web Container
+    </div>
+  );
+}
+
+export default WebContainer;
