c4f43cfdd79d7e810c705d576aabff796d4ac5ff
mattbf <matthewbf8@gmail.com>
2019-07-10 10:09:15 -0600

    Made colour picker a functional component

--- src/Drawing/ColourPicker.js
+++ src/Drawing/ColourPicker.js
@@ -1,42 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { ChromePicker } from 'react-color';
 import { CustomPicker } from 'react-color';
 
-class MyColorPicker extends React.Component {
-  state = {
-    displayColorPicker: false,
-  };
-
-  handleClick = () => {
-    this.setState({ displayColorPicker: !this.state.displayColorPicker })
-  };
-
-  handleClose = () => {
-    this.setState({ displayColorPicker: false })
-  };
-
-  render() {
-    const popover = {
-      position: 'absolute',
-      zIndex: '2',
-    }
-    const cover = {
-      position: 'fixed',
-      top: '0px',
-      right: '0px',
-      bottom: '0px',
-      left: '0px',
-    }
-    return (
-      <div>
-        <button onClick={ this.handleClick }>Pick Color</button>
-        { this.state.displayColorPicker ? <div style={ popover }>
-          <div style={ cover } onClick={ this.handleClose }/>
-          <ChromePicker />
-        </div> : null }
-      </div>
-    )
+function MyColorPicker() {
+  const [pickerOpen, setPickerOpen] = useState(false)
+  // state = {
+  //   displayColorPicker: false,
+  // };
+  function handleClick() {
+    setPickerOpen(!pickerOpen)
   }
+  function handleClose() {
+    setPickerOpen(false)
+  }
+  // handleClick = () => {
+  //   this.setState({ displayColorPicker: !this.state.displayColorPicker })
+  // };
+  //
+  // handleClose = () => {
+  //   this.setState({ displayColorPicker: false })
+  // };
+  const popover = {
+    position: 'absolute',
+    zIndex: '2',
+  }
+  const cover = {
+    position: 'fixed',
+    top: '0px',
+    right: '0px',
+    bottom: '0px',
+    left: '0px',
+  }
+  return (
+    <div>
+      <button onClick={handleClick}>Pick Color</button>
+      { pickerOpen ? <div style={ popover }>
+        <div style={ cover } onClick={handleClose}/>
+        <ChromePicker />
+      </div> : null }
+    </div>
+  )
 }
 
 export default CustomPicker(MyColorPicker);
